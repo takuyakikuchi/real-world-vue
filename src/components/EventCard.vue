a7ec2161cf8a1b8ca5ff4f44a8f144240c43158c
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-04-19 00:39:14 +0900

    [CSS](Card)Added hover scale

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -3,7 +3,9 @@
     :to="{ name: 'event-show', params: { id: event.id } }"
     class="w-1/2"
   >
-    <div class="rounded shadow-lg hover:bg-gray-200 bg-gray-100">
+    <div
+      class="rounded shadow-lg bg-gray-100 transform hover:shadow-2xl hover:scale-110"
+    >
       <div class="px-6 py-4">
         <div class="font-bold text-xl mb-2">{{ event.title }}</div>
         <p class="text-gray-700 text-base">@{{ event.date }}</p>
