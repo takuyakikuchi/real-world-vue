3539ba4813dbe2b36fdd93c1e3865b2ce1aeb57b
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-05-04 11:53:10 +0900

    [REFACTOR]Add some comments

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import camelCase from 'lodash/camelCase'
 const requireComponent = require.context(
   // The relative path of the components folder
   './components',
-  // Whether or not to look in subfolders
+  // Whether or not to look in sub folders
   false,
   // The regular expression used to match base component filenames
   /Base[A-Z]\w+\.(vue|js)$/
