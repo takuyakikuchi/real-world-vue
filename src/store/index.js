5fbd1a2c33f7f8d694dd71476c51fce384c45bed
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-05-05 13:20:53 +0900

    [FEATURE]fetchEvents

--- src/store/index.js
+++ src/store/index.js
@@ -29,6 +29,9 @@ export default new Vuex.Store({
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
     }
   },
   actions: {
@@ -36,6 +39,15 @@ export default new Vuex.Store({
       return EventService.postEvent(event).then(() => {
         commit('ADD_EVENT', event.data)
       })
+    },
+    fetchEvents({ commit }) {
+      return EventService.getEvents()
+        .then(response => {
+          commit('SET_EVENTS', response.data)
+        })
+        .catch(error => {
+          console.log('There was an error:', error.response)
+        })
     }
   },
   modules: {}
